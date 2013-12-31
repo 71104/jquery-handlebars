628e34933b4000773fd56955be99f1ff0bb5bed0
marshalys <marshalys@gmail.com>
2013-12-31 16:22:22 +0800

    add support for inline templates

--- src/plugin.js
+++ src/plugin.js
@@ -35,6 +35,22 @@
 		}, 'text');
 	}
 
+    function getCacheKey(templateName) {
+        return 'cache_' + templateName;
+    }
+
+    function render($this, templateName, data) {
+        var cacheKey = getCacheKey(templateName);
+        var template = cache[cacheKey];
+        var content = template(data);
+        $this.html(content).trigger('render.handlebars', [templateName, data]);
+    }
+
+    function cacheTemplate(templateName, templateContent) {
+        var cacheKey = getCacheKey(templateName);
+        cache[cacheKey] = Handlebars.compile(templateContent);
+    }
+
 	$.handlebars = function () {
 		if (typeof arguments[0] !== 'string') {
 			var options = arguments[0];
@@ -71,16 +87,28 @@
 	};
 
 	$.fn.render = function (templateName, data) {
-		var url = resolveTemplatePath(templateName);
-		if (cache.hasOwnProperty(url)) {
-			this.html(cache[url](data)).trigger('render.handlebars', [templateName, data]);
-		} else {
-			var $this = this;
-			$.get(url, function (template) {
-				cache[url] = Handlebars.compile(template);
-				$this.html(cache[url](data)).trigger('render.handlebars', [templateName, data]);
-			}, 'text');
-		}
+        var cacheKey = getCacheKey(templateName);
+        if (cache.hasOwnProperty(cacheKey)) {
+            render(this, templateName, data);
+            return this;
+        }
+        var promise = null;
+        if (templateName.charAt(0) === '#') {
+            var defer = $.Deferred();
+            promise = defer.promise();
+            defer.resolve($(templateName).html());
+        }
+        else {
+            promise = $.ajax({
+                url: resolveTemplatePath(templateName),
+                dataType: 'text'
+            });
+        }
+        var $this = this;
+        promise.done(function (template) {
+            cacheTemplate(templateName, template);
+            render($this, templateName, data);
+        });
 		return this;
 	};
 }(jQuery));
